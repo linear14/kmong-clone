36ff6548d68bd0a78cdc727c427731efbbaa7f00
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2021-12-24 17:14:13 +0900

    :wrench: 클라이언트 Webpack5 설정 및 테스트

---
+++ client/webpack.config.js
@@ -0,0 +1,51 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: '[name].[chunkhash].js',
+    assetModuleFilename: pathData => {
+      const filepath = path
+        .dirname(pathData.filename)
+        .split('/')
+        .slice(1)
+        .join('/');
+      return `${filepath}/[hash][ext]`;
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        type: 'asset'
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin()
+  ],
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    open: true
+  }
+};
