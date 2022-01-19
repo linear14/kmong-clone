121d50602cf6ad9e542f1dc1692bb51b48d68997
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-01-19 21:08:07 +0900

    :art: CategoryNav 내부 NavItem Container 구조 변경

--- client/src/components/common/GNB/CategoryNav/NavItems/CategoryBox.tsx
+++ client/src/components/common/GNB/CategoryNav/NavItems/CategoryBox.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { flexBox } from 'src/util/style';
 import styled from 'styled-components';
 import { MdMenu, MdArrowDropDown } from 'react-icons/md';
+import { FlexDirection } from 'src/enum/style';
 
 const Container = styled.div`
-  ${flexBox}
+  ${flexBox({ direction: FlexDirection.COLUMN })};
   height: 100%;
+  cursor: pointer;
+`;
 
+const InnerContainer = styled.div`
+  ${flexBox}
+  flex: 1;
   font-size: 16px;
   font-weight: bold;
-  cursor: pointer;
 
   span {
     margin-left: 8px;
@@ -21,9 +26,11 @@ const Container = styled.div`
 const CategoryBox = () => {
   return (
     <Container>
-      <MdMenu size={24} />
-      <span>전체 카테고리</span>
-      <MdArrowDropDown size={16} />
+      <InnerContainer>
+        <MdMenu size={24} />
+        <span>전체 카테고리</span>
+        <MdArrowDropDown size={16} />
+      </InnerContainer>
     </Container>
   );
 };
--- client/src/components/common/GNB/CategoryNav/NavItems/EnterpriseBox.tsx
+++ client/src/components/common/GNB/CategoryNav/NavItems/EnterpriseBox.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FlexDirection } from 'src/enum/style';
 import { flexBox } from 'src/util/style';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  ${flexBox({ direction: FlexDirection.COLUMN })};
+  margin-left: 40px;
+  height: 100%;
+  cursor: pointer;
+`;
+
+const InnerContainer = styled.div`
+  ${flexBox}
+  flex: 1;
+`;
+
 const EnterpriseLink = styled(Link)`
   font-size: 16px;
+
   &::after {
     content: '엔터프라이즈';
   }
 `;
 
-const Container = styled.div`
-  ${flexBox}
-  margin-left: 40px;
-  height: 100%;
-`;
-
 const EnterpriseBox = () => {
+
   return (
     <Container>
-      <EnterpriseLink to='/' />
+      <InnerContainer>
+        <EnterpriseLink to='/' />
+      </InnerContainer>
     </Container>
   );
 };
