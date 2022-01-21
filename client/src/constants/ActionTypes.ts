69ef8ad13eb7734f4c219effa117185b7f9ad6a0
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-01-21 21:44:54 +0900

    :sparkles: Category 조회 관련 action type 생성

---
+++ client/src/constants/ActionTypes.ts
@@ -0,0 +1,3 @@
+export const GET_ALL_CATEGORIES = 'category/GET_ALL' as const;
+export const GET_ALL_CATEGORIES_SUCCESS = 'category/GET_ALL_SUCCESS' as const;
+export const GET_ALL_CATEGORIES_ERROR = 'categroy/GET_ALL_ERROR' as const;
