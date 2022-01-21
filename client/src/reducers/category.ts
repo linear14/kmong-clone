df0c742b42c6a08139f36949be75f05a46d34625
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-01-21 21:55:30 +0900

    :sparkles: Category 상태 조작 Reducer 및 rootReducer로 combine

--- client/src/reducers/category.ts
+++ client/src/reducers/category.ts
@@ -1,3 +1,10 @@
+import {
+  GET_ALL_CATEGORIES,
+  GET_ALL_CATEGORIES_ERROR,
+  GET_ALL_CATEGORIES_SUCCESS
+} from 'src/constants/ActionTypes';
+import produce from 'immer';
+import { ActionRequest } from 'src/actions/category';
 import { ICategoryState } from 'src/types/category';
 
 const initState: ICategoryState = {
@@ -5,3 +12,27 @@ const initState: ICategoryState = {
   categories: []
 };
 
+export const categoryList = (state = initState, action: ActionRequest) => {
+  switch (action.type) {
+    case GET_ALL_CATEGORIES: {
+      return produce(state, draft => {
+        draft.state.loading = true;
+      });
+    }
+    case GET_ALL_CATEGORIES_SUCCESS: {
+      return produce(state, draft => {
+        draft.state.loading = false;
+        draft.categories = action.payload.categories;
+      });
+    }
+    case GET_ALL_CATEGORIES_ERROR: {
+      return produce(state, draft => {
+        draft.state.loading = false;
+        draft.categories = [];
+        draft.state.error = action.payload.error;
+      });
+    }
+    default:
+      return state;
+  }
+};
