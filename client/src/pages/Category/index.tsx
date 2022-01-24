9a7a2c3bb334d06ff0a71356e8a2c43de7ffa019
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-01-24 21:33:04 +0900

    :sparkles: 카테고리 페이지 Body 영역 분할

--- client/src/pages/Category/index.tsx
+++ client/src/pages/Category/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Body, SideNav } from 'src/components/Category';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 100%;
-  height: 400px;
+  width: 1170px;
+  min-width: 1170px;
+  height: 100%;
+  margin: 40px 0px;
+
+  display: flex;
 `;
 
 const CategoryPage = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <SideNav />
+      <Body />
+    </Container>
+  );
 };
 
 export default CategoryPage;
--- client/src/pages/common/Body/index.tsx
+++ client/src/pages/common/Body/index.tsx
@@ -3,7 +3,11 @@ import styled from 'styled-components';
 
 const BodyContainer = styled.div`
   width: 100%;
+  min-width: 1170px;
   position: relative;
+
+  display: flex;
+  justify-content: center;
 `;
 
 const Body = ({ children }: { children: React.ReactNode }) => {
