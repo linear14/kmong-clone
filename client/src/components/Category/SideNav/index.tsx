e7af60038ac991ed12c7dd76ec2f27f62500e9ec
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-01-27 17:00:56 +0900

    :sparkles: 라우팅 및 라우팅 확인을 위한 SideNav 스타일 속성 일부 변경

--- client/src/components/Category/SideNav/index.tsx
+++ client/src/components/Category/SideNav/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
 import { useRootCategoryState } from 'src/hooks';
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
   width: 200px;
@@ -17,17 +18,19 @@ const RootTitle = styled.div`
   border-bottom: 2px solid #fcd200;
 `;
 
-const Category = styled.div`
+const Category = styled(Link)<{ $isActive: boolean }>`
   width: 100%;
   height: 32px;
   line-height: 32px;
+  display: block;
   font-size: 14px;
-  color: #555969;
+  font-weight: ${({ $isActive }) => ($isActive ? 'bold' : 500)};
+  color: ${({ $isActive }) => ($isActive ? 'black' : '#555969')};
   cursor: pointer;
 
   &:hover {
     color: #212224;
-    font-weight: 500;
+    font-weight: bold;
   }
 `;
 
@@ -43,7 +46,13 @@ const SideNav = () => {
         <>
           <RootTitle>{rootCategory.name}</RootTitle>
           {rootCategory.children?.map(item => (
-            <Category key={item.id}>{item.name}</Category>
+            <Category
+              key={item.id}
+              $isActive={item.id === Number(categoryIdx)}
+              to={`/category/${item.id}`}
+            >
+              {item.name}
+            </Category>
           ))}
         </>
       )}
