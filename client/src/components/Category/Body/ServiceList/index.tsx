37669b21603412cadd3f0762ef53736b32522685
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-01-30 16:19:22 +0900

    :fire: 샘플 ServiceList 삭제

--- client/src/components/Category/Body/ServiceList/index.tsx
+++ client/src/components/Category/Body/ServiceList/index.tsx
@@ -9,26 +9,8 @@ const Container = styled.div`
   flex-wrap: wrap;
 `;
 
-const ServiceList = () => {
-  const sampleService = {
-    serviceIdx: 1,
-    nickname: '동현',
-    level: 2,
-    title: '비전공 프론트엔드 개발자의 공부 방법을 공유해 드립니다.',
-    isLike: true,
-    price: 30000,
-    itemCnt: 2,
-    rate: 4.8,
-    rateCnt: 15
-  };
-
   return (
     <Container>
-      <ServiceCard service={sampleService} />
-      <ServiceCard service={sampleService} />
-      <ServiceCard service={sampleService} />
-      <ServiceCard service={sampleService} />
-      <ServiceCard service={sampleService} />
     </Container>
   );
 };
