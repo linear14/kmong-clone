95815fe2bb792402fb7e0aa85d69fd76eb2ca758
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-02-06 15:50:47 +0900

    🤡 서비스 상세화면 구현을 위한 Mock 데이터 준비 (user, service, serviceItem)

---
+++ client/src/__mock__/user.ts
@@ -0,0 +1,38 @@
+export const userMock = [
+  {
+    userIdx: 1,
+    nickname: '테스트유저1',
+    description: '테스트유저1의 프로필입니다',
+    location: '경기',
+    specialities: '1',
+    skills: '1',
+    schools: '경희대학교',
+    canStay: 1,
+    hopeSalary: 10000,
+    profileImageUrl: null,
+    email: 'linear14@naver.com',
+    signMethod: 'test',
+    password: '1234',
+    isExpert: 1,
+    isLoginState: 0,
+    level: 0
+  },
+  {
+    userIdx: 2,
+    nickname: '테스트유저2',
+    description: '테스트유저2의 프로필입니다',
+    location: '경기',
+    specialities: '1',
+    skills: '1',
+    schools: '경희대학교',
+    canStay: 0,
+    hopeSalary: 20000,
+    profileImageUrl: null,
+    email: 'linear2@naver.com',
+    signMethod: 'test',
+    password: '1234',
+    isExpert: 1,
+    isLoginState: 0,
+    level: 1
+  }
+];
