fae570727ef6217f7568ae454201a961fb57eb55
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-02-06 13:45:45 +0900

    :lipstick: 서비스 상세 페이지 컨테이너 레이아웃 지정

--- client/src/pages/Service/index.tsx
+++ client/src/pages/Service/index.tsx
@@ -2,9 +2,34 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  width: 1170px;
+  min-width: 1170px;
+  padding: 24px 0px 48px;
+  display: flex;
+  position: relative;
+`;
+
+const BodyLeft = styled.div`
+  flex: 1;
+  position: relative;
+`;
+
+const BodyRight = styled.div`
+  width: 436px;
+  margin-left: 48px;
+  position: relative;
+`;
+
 const Service = () => {
   const { serviceIdx } = useParams();
-  return <div>{serviceIdx}</div>;
+
+  return (
+    <Container>
+      <BodyLeft />
+      <BodyRight />
+    </Container>
+  );
 };
 
 export default Service;
